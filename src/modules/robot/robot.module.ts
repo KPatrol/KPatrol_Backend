d829ff051863c078dcaaf8d89bcf2faa869584ce
Vu Dang Khoa <khoabuit0910@gmail.com>
2026-05-04 08:09:03 +0700

    refactor(robot): module wiring with notification dependency

--- src/modules/robot/robot.module.ts
+++ src/modules/robot/robot.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { RobotService } from './robot.service';
 import { RobotController } from './robot.controller';
+import { getStreamTokenSecret } from '../../config/auth.config';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: getStreamTokenSecret(),
+    }),
+  ],
   providers: [RobotService],
   controllers: [RobotController],
   exports: [RobotService],
