c428e591f482729bfaed6ba0cbd0a73c1449e8d1
Vu Dang Khoa <khoabuit0910@gmail.com>
2026-05-04 08:09:03 +0700

    refactor(socket): wire rate limiter into module

--- src/modules/socket/socket.module.ts
+++ src/modules/socket/socket.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { SocketGateway } from './socket.gateway';
 import { RobotModule } from '../robot/robot.module';
+import { getJwtSecret } from '../../config/auth.config';
 
 @Module({
-  imports: [RobotModule],
+  imports: [
+    RobotModule,
+    JwtModule.register({
+      secret: getJwtSecret(),
+    }),
+  ],
   providers: [SocketGateway],
   exports: [SocketGateway],
 })
