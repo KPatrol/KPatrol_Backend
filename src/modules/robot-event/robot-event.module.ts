e89b346e37d75541d6fb0c05d29d3494ee89d88f
Vu Dang Khoa <khoabuit0910@gmail.com>
2026-05-04 08:09:03 +0700

    refactor(robot-event): module wiring

--- src/modules/robot-event/robot-event.module.ts
+++ src/modules/robot-event/robot-event.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { RobotEventController } from './robot-event.controller';
 import { RobotEventService } from './robot-event.service';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { NotificationModule } from '../notification/notification.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, NotificationModule],
   controllers: [RobotEventController],
   providers: [RobotEventService],
   exports: [RobotEventService],
