122bd99349b934f49d82d6627bb02fe2c9122b46
Vu Dang Khoa <khoabuit0910@gmail.com>
2026-05-04 08:08:56 +0700

    refactor(notification): wire channels into module

--- src/modules/notification/notification.module.ts
+++ src/modules/notification/notification.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
+import { EmailChannel } from './channels/email.channel';
+import { ZaloChannel } from './channels/zalo.channel';
 
 @Module({
-  providers: [NotificationService],
+  providers: [NotificationService, EmailChannel, ZaloChannel],
   controllers: [NotificationController],
   exports: [NotificationService],
 })
